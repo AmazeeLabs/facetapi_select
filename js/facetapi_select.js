94fc54222e8e837bbf5f02e1dfff7e376a1e8e0c
Alexander Tkachev <alex.leksat@gmail.com>
2013-09-09 11:23:32 +0400

    Applied path from https://drupal.org/node/2084661#comment-7840145 (Allow unbind onchange handler)

--- js/facetapi_select.js
+++ js/facetapi_select.js
@@ -3,19 +3,19 @@
     attach: function(context, settings) {
       if (settings.facetapi) {
         // We do a cheeky window.location to skip a form submission
-        var goToLocation = function() {
-          var location = $(this).find('.form-select').val();
-          if (location) {
-            window.location.href = location;
-          }
-        };
         for (var index in settings.facetapi.facets) {
           var facet = settings.facetapi.facets[index];
           if (facet.widget === 'facetapi_select_dropdowns' && facet.autoSubmit === 1) {
             // Add a handler for our select boxes
-            $('#' + facet.id).change(goToLocation);
+            $('#' + facet.id).change(Drupal.behaviors.facetapiSelect.goToLocation);
           }
         }
+      }
+    },
+    goToLocation: function() {
+      var location = $(this).find('.form-select').val();
+      if (location) {
+        window.location.href = location;
       }
     }
   };
