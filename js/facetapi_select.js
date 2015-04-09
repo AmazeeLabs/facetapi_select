94e7c0b301deb81865454428221a0b521cce1c53
Claudine Braendle <claudine@amazeelabs.com>
2015-04-09 10:30:49 +0200

    applied path from https://www.drupal.org/node/2118323#comment-8940547

--- js/facetapi_select.js
+++ js/facetapi_select.js
@@ -1,8 +1,12 @@
+/**
+ * @file
+ * Handles Facet API Select ajax functionality.
+ */
 (function ($) {
   Drupal.behaviors.facetapiSelect = {
     attach: function(context) {
       $(".facetapi-select-submit").hide();
     }
-  }; 
+  };
 })(jQuery);
   
